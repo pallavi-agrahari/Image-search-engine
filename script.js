abf843846e2ac1e08ffa9f1e887395a9ccef5ab3
pallavi-agrahari <141801184+pallavi-agrahari@users.noreply.github.com>
2024-09-16 14:25:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ let page = 1;
 
 async function searchImages(){
     keyword = searchBox.value;
-    const url = `https://api.unsplash.com/search/photos?page=1&query=office&client_id=SrJc722HczBocdoX5AiH7gnHaOve1zxxte1tbRHQhpw`;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}`;
 
     const response = await fetch(url);
     const data = await response.json();
